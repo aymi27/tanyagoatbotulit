7f13165f3485efd224deb3bab7d7a757b86721a7
yoya <162847989+aymi18@users.noreply.github.com>
2024-03-29 21:57:13 +0800

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ♥️| 𝙔𝘼𝙕𝙆𝙔 𝘼𝙄 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ ♥ | 𝙏𝘼𝙉𝙔𝘼 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
 	config: {
@@ -34,8 +34,8 @@ module.exports = {
 			const categories = {};
 			let msg = "";
 
-			msg += `░░░░█▄█ ▄▀█ ▀█ █▄▀ █▄█░░░
-░░░░░█░ █▀█ █▄ █░█ ░█░░░░\n\n◦❭❯❱ 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 & 𝗖𝗔𝗧𝗘𝗚𝗢𝗥𝗬 ❰❮❬◦`; // replace with your name 
+			msg += `█▓▒­░⡷🧜‍♀️𝗧𝗔𝗡𝗬𝗔🧜‍♀️⢾░▒▓█
+▄▀▄ 𝗈𝗐𝗇𝖾𝗋 𝗄𝗈𝗇𝗀 𝗆𝖺𝗀𝖺𝗇𝖽𝖺: 𝗬𝗢𝗬𝗔 ▀▄▀\n\n◦❭❯❱ 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 & 𝗖𝗔𝗧𝗘𝗚𝗢𝗥𝗬 ❰❮❬◦`; // replace with your name 
 
 			for (const [name, value] of commands) {
 				if (value.config.role > 1 && role < value.config.role) continue;
