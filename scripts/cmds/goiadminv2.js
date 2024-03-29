973de65ee8923d464d3735041f9c3607223de851
yoya <162847989+aymi18@users.noreply.github.com>
2024-03-29 19:12:09 +0800

    Update goiadminv2.js

--- scripts/cmds/goiadminv2.js
+++ scripts/cmds/goiadminv2.js
@@ -12,16 +12,16 @@ module.exports = {
 	onStart: async function () {},
 	onChat: async function ({ event, message, getLang, api }) {
 		const msg = [
-			"Stop mentioning my creator, he's busy 😗",
-			"My Creator is currently offline 😢",
-			"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
+			"Wala tulog pa yun! ",
+			"My master is currently offline 😢",
+			"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 master, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
 			"busy pa ata yun kaya mag-antay ka",
-			"Sorry, naka bebetime pa don't disturb him 🙄",
-			"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/swordigo.swordslush 😏",
-			"Another tag in my Creator, i will kick your fucking ass"
+			"Sorry, naka bebetime pa don't disturb her 🙄",
+			"Do you like my master thats why your tagging her? Why dont you add him  https://www.facebook.com/100095262681590 😏",
+			"Another tag in my master, i will kick your fucking ass"
 		];
 
-		const CliffRegex = /^(@Cliff Vincent V. Torrevillas 󱢏)$/i;
+		const CliffRegex = /^(@yoya 󱢏)$/i;
 		if (event.body && CliffRegex.test(event.body)) {
 			api.setMessageReaction("😍", event.messageID, (err) => {}, true);
 			return api.sendMessage({ body: msg[Math.floor(Math.random() * msg.length)] }, event.threadID, event.messageID);
