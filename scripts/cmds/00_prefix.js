d7af427e1739e6b1c3772f5dfc3f92a8d6ecd07a
yoya <162847989+aymi18@users.noreply.github.com>
2024-04-01 18:04:04 +0800

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -15,12 +15,12 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 ! 𓆪 ]\n
+──────▄▀▄─────▄▀▄\n─────▄█░░▀▀▀▀▀░░█▄\n─▄▄──█░░░░░░░░░░░█──▄▄\n█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█\n▀█▀ ▄▀█ █▄░█ █▄█ ▄▀█\n░█░ █▀█ █░▀█ ░█░ █▀█\nHello this is Tanya! Ang Magandang BOT ni Yoya/Aymi  [ 𓆩 ! 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥ !help [number of page] -> see commands
 ➥ !sim [message] -> talk to bot
 ➥ !callad [message] -> report any problem encountered
-➥ help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/100095262681590`,
+➥ !help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/100095262681590`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
  });
  }
